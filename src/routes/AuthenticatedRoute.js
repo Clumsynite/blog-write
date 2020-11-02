44f301f0f3742b07d7eef34c9362610e8e3d5faf
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-02 18:43:31 +0530

    created authenticated route
    same as blog read

---
+++ src/routes/AuthenticatedRoute.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+
+const AuthenticatedRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        localStorage.getItem("token") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+};
+
+export default AuthenticatedRoute;
