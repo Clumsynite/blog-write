73a7ef519ba2883c488b30a4cd06fa2df2112cf4
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-02 20:09:23 +0530

    Created AddPost component to add post using TinyMCE editor

---
+++ src/components/AddPost.js
@@ -0,0 +1,12 @@
+import { Editor } from "@tinymce/tinymce-react";
+import { Markup } from "interweave";
+
+const AddPost = () => {
+  return(
+    <div className="AddPost">
+      
+    </div>
+  )
+};
+
+export default AddPost;
