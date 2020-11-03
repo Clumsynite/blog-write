3bad727ec39cb2d5f106b1cef8d9abd7da30c438
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-11-03 16:23:18 +0530

    Added Component to use as Editor

---
+++ src/templates/TinyMCE.js
@@ -0,0 +1,28 @@
+import { Editor } from "@tinymce/tinymce-react";
+
+const TintMCE = (props) => {
+  return (
+    <Editor
+      apiKey="iegn84mumhivsy1it2lvc8qjfxkaav0snoxsx4u66dxlnz3g"
+      initialValue=""
+      init={{
+        height: props.height,
+        menubar: false,
+        plugins: [
+          "advlist autolink lists link image charmap print preview anchor",
+          "searchreplace visualblocks code fullscreen",
+          "insertdatetime media table paste code help wordcount",
+        ],
+        toolbar:
+          "undo redo | formatselect | bold italic backcolor | " +
+          "alignleft aligncenter alignright alignjustify | " +
+          "bullist numlist outdent indent | removeformat | help",
+        placeholder: props.placeholder,
+      }}
+      onEditorChange={props.handleChange}
+      value={props.value}
+    />
+  );
+};
+
+export default TinyMCE;
